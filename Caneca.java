839120ec4961171c3b8093e39d80489e02be584f
Eduardoui <eduardobacelar2007@gmail.com>
2026-02-20 17:43:57 -0300

    Add files via upload

---
+++ Caneca.java
@@ -0,0 +1,14 @@
+package br.com.fiapride.model;
+
+public class Caneca {
+	
+	public String cor;
+	public String tamanho;
+	public String material;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
